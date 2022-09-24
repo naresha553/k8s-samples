02399da130ada8f83af4274d4f2e56feea5bfcb4
naresha553 <naresha553@gmail.com>
2022-09-24 10:04:01 -0400

    commit1

--- Initialization-Script.sh
+++ Initialization-Script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+rm -rf /home/cloud_user/k8s-samples
 git clone https://github.com/naresha553/k8s-samples.git
 terraform -chdir=/home/cloud_user/k8s-samples/lab-terraform-eks init
 terraform -chdir=/home/cloud_user/k8s-samples/lab-terraform-eks plan
 terraform -chdir=/home/cloud_user/k8s-samples/lab-terraform-eks apply --auto-approve
-aws eks --region $(terraform -chdir=/home/cloud_user/k8s-samples/lab-terraform-eks output -raw region) update-kubeconfig --name $(terraform -chdir=/home/cloud_user/k8s-samples/lab-terraform-eks output -raw cluster_name)
+sh /home/cloud_user/kuinstall.sh
 wget https://raw.githubusercontent.com/linuxacademy/content-terraform-2021/main/lab_kubernetes_resources.tf -P /home/cloud_user/k8s-samples/lab-terraform-eks
+kubectl apply -f /home/cloud_user/k8s-samples/
