f2bf18fddd6fef9e17db02c3ea7d33e46984005f
naresha553 <naresha553@gmail.com>
2022-09-26 22:28:21 -0400

    Update Initialization-Script.sh

--- Initialization-Script.sh
+++ Initialization-Script.sh
@@ -9,4 +9,4 @@ terraform -chdir=/home/cloud_user/k8s-samples/aws-terraform-instances/terraform-
 terraform -chdir=/home/cloud_user/k8s-samples/aws-terraform-instances/terraform-aws-examples-master/01-sample-instance apply --auto-approve
 sh /home/cloud_user/kuinstall.sh
 wget https://raw.githubusercontent.com/linuxacademy/content-terraform-2021/main/lab_kubernetes_resources.tf -P /home/cloud_user/k8s-samples/lab-terraform-eks
-kubectl apply -f /home/cloud_user/k8s-samples/
+kubectl apply -f /home/cloud_user/k8s-samples/froster/
